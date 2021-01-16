a1a99fbcc3e3f67de234e7ba66995272ab6a3d55
Константин <kostyad2005@gmail.com>
2021-01-16 22:35:44 +0300

    first commit by Kostya

---
+++ 3_file.py
@@ -0,0 +1,134 @@
+import pygame
+import random
+import os
+from os import path
+
+WIDTH = 1920
+HEIGHT = 1020
+FPS = 30
+
+# Задаем цвета
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        global player_imgs
+        self.imgs = player_imgs
+        self.animcount = 0
+        self.invert = False
+        player_img = pygame.transform.scale(player_imgs[0], (player_imgs[0].get_size()[0] * 3,
+                                                             player_imgs[0].get_size()[1] * 3))
+        self.image = player_img
+        self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+        self.speedx = 0
+
+    def update(self):
+        keystate = pygame.key.get_pressed()
+        self.speedx = 0
+        if keystate[pygame.K_LEFT]:
+            self.speedx = -10
+            self.animcount += 1
+            self.invert = True
+        elif keystate[pygame.K_RIGHT]:
+            self.invert = False
+            self.speedx = 10
+            self.animcount += 1
+        else:
+            self.animcount = 0
+        if self.animcount >= 30.00:
+            self.animcount = 0
+        a = self.animcount // 6
+        player_img = pygame.transform.scale(self.imgs[a], (self.imgs[a].get_size()[0] * 3,
+                                                           self.imgs[a].get_size()[1] * 3))
+        player_img = pygame.transform.flip(player_img, self.invert, False)
+        self.image = player_img
+        self.image.set_colorkey(BLACK)
+        x, y = self.rect.center
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+        self.rect.x += self.speedx
+
+
+class Float(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = dirt_image
+        self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+
+
+class Camera:
+    # зададим начальный сдвиг камеры
+    def __init__(self):
+        self.dx = 0
+        self.dy = 0
+
+    # сдвинуть объект obj на смещение камеры
+    def apply(self, obj):
+        obj.rect.x += self.dx
+        obj.rect.y += self.dy
+
+    # позиционировать камеру на объекте target
+    def update(self, target):
+        self.dx = -(target.rect.x + target.rect.w // 2 - WIDTH // 2)
+        self.dy = -(target.rect.y + target.rect.h - HEIGHT + 80)
+
+
+class Weapon:
+    pass
+
+
+# Создаем игру и окно
+img_dir = path.join(path.dirname(__file__), 'img')
+pygame.init()
+pygame.mixer.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+background = pygame.image.load(path.join(img_dir, "fon.png")).convert()
+background = pygame.transform.scale(background, (1920, 1020))
+background_rect = background.get_rect()
+pygame.display.set_caption("My Game")
+player_imgs = [pygame.image.load('img/pb1.png').convert(), pygame.image.load('img/pb2.png').convert(),
+               pygame.image.load('img/pb3.png').convert(), pygame.image.load('img/pb4.png').convert(),
+               pygame.image.load('img/pb5.png').convert()]
+dirt_image = pygame.image.load('img/dirt.png').convert()
+clock = pygame.time.Clock()
+all_sprites = pygame.sprite.Group()
+camera = Camera()
+for i in range(25):
+    dirt = Float(100 * i, 1050)
+    all_sprites.add(dirt)
+player = Player(200, 900)
+all_sprites.add(player)
+
+# Цикл игры
+running = True
+left = False
+while running:
+    # Держим цикл на правильной скорости
+    clock.tick(FPS)
+    # Ввод процесса (события)
+    for event in pygame.event.get():
+        all_sprites.update()
+        if event.type == pygame.QUIT:
+            running = False
+    # Обновление
+    all_sprites.update()
+    # Рендеринг
+    screen.fill(BLACK)
+    screen.blit(background, background_rect)
+    for sprite in all_sprites:
+        camera.apply(sprite)
+    camera.update(player)
+    all_sprites.draw(screen)
+    pygame.display.flip()
+
+pygame.quit()
